b550f62d54cb1e474e31040e67607e99784935b0
Amaro <amaro.txt@gmail.com>
2017-08-23 01:55:46 -0300

    GameController criação do percurso

---
+++ Assets/Scripts/InGame/ControllerCreatePlataforms.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCreatePlataforms : MonoBehaviour {
+
+	GameObject player;
+	GameObject up, down, left, right; 
+
+	List<GameObject> lines;
+	Vector3 lastDirection;
+
+	Vector3 positionPlayer, lastLinePosition, nextLinePosition ;
+		
+	// Update is called once per frame
+	void Start () {
+		lines = new List<GameObject>();
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+		StartLines ();
+	
+
+//		Instantiate (left);
+//		Instantiate (up);
+//		Instantiate (down);
+		lastLinePosition = new Vector3 (0,0,0);
+	}
+
+	void FixedUpdate(){
+//		if (lines.Count <= 10){
+//			CreatesNextLine ();
+//		}
+	}
+	void CreateNextLine(){
+		int randomDirection = Random.Range(0,100);
+		//up
+
+		if (lastDirection.z == 0) {
+			if (randomDirection <= 33){
+				InstantiateLines (left, new Vector3(0,4,0));
+			}
+			if (randomDirection > 33 && randomDirection <= 67){
+				InstantiateLines (up, new Vector3(0,4,0));
+			}
+			if (randomDirection > 67 && randomDirection <= 100){
+				InstantiateLines (right, new Vector3(0,4,0));
+			}
+		}
+		//right
+		if (lastDirection.z == 270) {
+			if (randomDirection <= 33){
+				InstantiateLines (up, new Vector3(4,0,0));
+			}
+			if (randomDirection > 33 && randomDirection <= 67){
+				InstantiateLines (right, new Vector3(4,0,0));
+			}
+			if (randomDirection > 67 && randomDirection <= 100){
+				InstantiateLines (down, new Vector3(4,0,0));
+			}
+		}
+		//down
+		if (lastDirection.z == 180) {
+			if (randomDirection <= 33){
+				InstantiateLines (right, new Vector3(0,-4,0));
+			}
+			if (randomDirection > 33 && randomDirection <= 67){
+				InstantiateLines (down, new Vector3(0,-4,0));
+			}
+			if (randomDirection > 67 && randomDirection <= 100){
+				InstantiateLines (left, new Vector3(0,-4,0));
+			}
+		}
+		//left
+		if (lastDirection.z == 90) {
+			if (randomDirection <= 33){
+				InstantiateLines (left, new Vector3(-4,0,0));
+			}
+			if (randomDirection > 33 && randomDirection <= 67){
+				InstantiateLines (down, new Vector3(-4,0,0));
+			}
+			if (randomDirection > 67 && randomDirection <= 100){
+				InstantiateLines (left, new Vector3(-4,0,0));
+			}
+		}
+	}
+
+	void InstantiateLines(GameObject line, Vector3 nextPosition){
+		nextLinePosition = lastLinePosition+nextPosition;
+		Debug.Log (nextPosition);
+		Instantiate (line,nextLinePosition, Quaternion.identity);
+
+		lastLinePosition = nextLinePosition;
+		lastDirection.z = line.transform.eulerAngles.z;
+	}
+
+	void StartLines(){
+		up = (GameObject)Resources.Load ("Prefabs/LinhaUp");;
+		down = (GameObject)Resources.Load ("Prefabs/LinhaDown");;
+		left = (GameObject)Resources.Load ("Prefabs/LinhaLeft");;
+		right = (GameObject)Resources.Load ("Prefabs/LinhaRight");;
+
+//		Debug.Log(right.transform.eulerAngles.z);
+//		Debug.Log(up.transform.eulerAngles.z);
+//		Debug.Log(down.transform.eulerAngles.z);
+//		Debug.Log(left.transform.eulerAngles.z);
+	}
+
+
+}
