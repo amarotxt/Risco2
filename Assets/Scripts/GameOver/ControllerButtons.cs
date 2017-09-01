07618563ed69a4a994d557aa02705bf01b203199
Amaro <amaro.txt@gmail.com>
2017-09-01 17:40:58 -0300

    Lerp para Movetowards no desenho do caminho

---
+++ Assets/Scripts/GameOver/ControllerButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllerButtons : MonoBehaviour {
+	GameObject playerMoviments;
+
+	public void NewGame(){
+		playerMoviments = GameObject.Find ("PlayerMoviments");
+		if (playerMoviments != null){
+			Destroy (playerMoviments);
+		}
+		SceneManager.LoadScene (0);
+	}
+}
--- Assets/Scripts/GameOver/MovePlayer.cs
+++ Assets/Scripts/GameOver/MovePlayer.cs
@@ -14,12 +14,14 @@ public class MovePlayer : MonoBehaviour {
 		notHaveMovments = true;
 		positionPlayerModify = new List<Vector3> ();
 		i = 0;
-		speed = 30f;
+		speed = 10;
+
 		playerMoviments = GameObject.Find ("PlayerMoviments");
 		if (playerMoviments != null){
 			savePositions = playerMoviments.GetComponent<PlayerMoviments>();
 			notHaveMovments = false;
 		}
+		positionPlayerModify.Clear ();
 		if (!notHaveMovments){
 			positionsPlayer = savePositions.PositionsMoviments;
 			foreach(Vector3 a in positionsPlayer){
@@ -31,10 +33,10 @@ public class MovePlayer : MonoBehaviour {
 //	 Update is called once per frame
 	void Update () {
 		if (!notHaveMovments) {
-			transform.position = Vector3.Lerp (transform.position, positionPlayerModify [i], Time.deltaTime * speed);
+			transform.position = Vector3.MoveTowards (transform.position, positionPlayerModify [i], Time.deltaTime * speed);
 			if (transform.position == positionPlayerModify [i] && i < positionPlayerModify.Count - 1) {
 				i += 1;
-				speed += 5f;
+				speed += 1f;
 			}
 		}
 	}
