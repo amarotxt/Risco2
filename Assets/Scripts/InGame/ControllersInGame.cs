f3262665ecfd393c2aac892da1eaf62806613fdc
Amaro <amaro.txt@gmail.com>
2017-09-28 20:37:51 -0300

    button pause, sounds, movimente

---
+++ Assets/Scripts/InGame/ControllersInGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllersInGame : MonoBehaviour {
+	public GameObject pauseButton;
+	public GameObject playButton;
+	public void PauseGame(){
+		pauseButton.SetActive (false);
+		playButton.SetActive (true);
+		Time.timeScale = 0;
+	}
+	public void PlayGame(){
+		pauseButton.SetActive (true);
+		playButton.SetActive (false);
+		Time.timeScale = 1;
+	}
+}
